ca54d250fe54b6b508fff599848a3549093d3c1e
Oxoo10 <oswaldkoutangni@gmail.com>
2024-12-20 17:47:07 +0300

    cavheMatrix Oswald

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinvmat <- function (solve) i <<- solve
+  getinvmat <- function() m
+  list (set = set, get = get,
+        setinvmat = setinvmat,
+        getinvmat = getinvmat)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinvmat()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinvmat(i)
+  i
 }
+
